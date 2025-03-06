9a7ea15ad8f680e2e586ac915db5168a21f29b37
El hadji <elhadjifallou070@gmail.com>
2025-03-06 12:07:16 +0000

    suppresion commentaire et creation des migration

--- backend_dekkway/dekkway/Dekkway_app/models.py
+++ backend_dekkway/dekkway/Dekkway_app/models.py
@@ -121,25 +121,3 @@ class Favoris(models.Model):
 
     def __str__(self):
         return f"{self.locataire} - {self.logement}"
-# class Service(models.Model):
-#     nom = models.CharField(max_length=100)
-#     description = models.TextField()
-#     prix = models.DecimalField(max_digits=10, decimal_places=2)
-#     disponibilite = models.BooleanField(default=True)
-#     date_ajout = models.DateTimeField(auto_now_add=True)
-#     date_modification = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         return self.nom
-
-# # Modèle Favoris (Logements ajoutés en favoris par les locataires)
-# class Favoris(models.Model):
-#     locataire = models.ForeignKey(Locataire, on_delete=models.CASCADE)
-#     logement = models.ForeignKey(Logement, on_delete=models.CASCADE)
-#     date_ajout = models.DateTimeField(auto_now_add=True)
-
-#     class Meta:
-#         unique_together = ("locataire", "logement")
-
-#     def __str__(self):
-#         return f"{self.locataire} - {self.logement}"
