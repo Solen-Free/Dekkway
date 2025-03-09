ac06f420202c1d5bb24b9f98b3e4ed9a2a114341
mouhadiol <122018731+mouhadiol@users.noreply.github.com>
2025-03-09 17:27:45 +0000

    j'ai fini la branche connexion inscription, connexion, mot de passe oublié, et recuperation de mot de passe

---
+++ frontend_dekkway/frontend/app/mot-de-passe-oublie/page.tsx
@@ -0,0 +1,6 @@
+// app/mot-de-passe-oublie/page.tsx
+import MotDePasseOublie from "@/components/MotDePasseOublie"; // Importez le composant
+
+export default function MotDePasseOubliePage() {
+  return <MotDePasseOublie />; // Utilisez le composant ici
+}
---
+++ frontend_dekkway/frontend/app/reinitialiser-mot-de-passe/page.tsx
@@ -0,0 +1,10 @@
+// app/reinitialiser-mot-de-passe/page.tsx
+import ResetPassword from "@/components/ResetPassword";
+
+export default function ResetPasswordPage() {
+  return (
+    <div>
+      <ResetPassword />
+    </div>
+  );
+}
