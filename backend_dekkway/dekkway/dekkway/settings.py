1dff0e1b4ce5af8bd9f2f8620e94153b81c9b253
El hadji <elhadjifallou070@gmail.com>
2025-03-11 14:34:28 +0000

    merge pour test

--- backend_dekkway/dekkway/dekkway/settings.py
+++ backend_dekkway/dekkway/dekkway/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_filters',
     'django.contrib.postgres',
     'rest_framework.authtoken',
+    "corsheaders",
 ]
 
 
@@ -66,7 +67,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # URL de ton frontend React/Next.js
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'dekkway.urls'
 
