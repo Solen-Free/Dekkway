157cb003a3783870bbeccc4285e9042dddf59f65
Chi8kamaru <serignecheikhfalldiop@gmail.com>
2025-03-13 12:15:15 +0000

    supression detailog

--- frontend_dekkway/frontend/components/chargemment.tsx
+++
@@ -1,9 +0,0 @@
-function LoadingSpinner() {
-    return (
-      <div className="flex justify-center items-center h-64">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#FC9B89]"></div>
-        if (loading) return <LoadingSpinner />;
-        if (!logement) return <div className="text-center text-red-500">Logement non trouvé</div>;
-      </div>
-    );
-  }
