b557b09aca462cce7ab835df76815a0628cc338c
mouhadiol <122018731+mouhadiol@users.noreply.github.com>
2025-03-20 15:32:48 +0000

    Update page.tsx

--- frontend_dekkway/frontend/app/servicessuplementaires/page.tsx
+++ frontend_dekkway/frontend/app/servicessuplementaires/page.tsx
@@ -13,10 +13,6 @@ const ServicePage: React.FC = () => {
     hover: { scale: 1.05, borderColor: '#014F86', transition: { duration: 0.2 } },
   };
 
-  const buttonVariants = {
-    hover: { scale: 1.05, backgroundColor: '#FC9B89', transition: { duration: 0.2 } },
-  };
-
   const handleOptionSelect = (option: string) => {
     setSelectedOption(option);
   };
@@ -24,19 +20,20 @@ const ServicePage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
       <motion.div
-        className="bg-white p-8 rounded-3xl shadow-md w-full max-w-2xl border-2 border-[#014F86]"
+        className="bg-white p-6 md:p-8 rounded-3xl shadow-md w-full max-w-2xl border-2 border-[#014F86]"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
       >
-        <h1 className="text-2xl font-bold text-center mb-6 text-[#014F86]">
+        <h1 className="text-2xl md:text-3xl font-bold text-center mb-6 text-[#014F86]">
           Merci de votre réservation !
         </h1>
-        <p className="text-gray-600 text-center mb-8">
+        <p className="text-gray-600 text-center mb-8 text-lg md:text-xl">
           Avez-vous besoin d'un service supplémentaire ?
         </p>
 
-        <div className="space-y-6">
+        <div className="space-y-4 md:space-y-6">
+          {/* Option Déménagement */}
           <Link href="/demenagement" passHref>
             <motion.label
               className={`flex flex-col md:flex-row items-center justify-between p-4 border-2 ${
@@ -48,7 +45,7 @@ const ServicePage: React.FC = () => {
               whileHover="hover"
               onClick={() => handleOptionSelect('demenagement')}
             >
-              <div className="flex items-center">
+              <div className="flex items-center w-full md:w-auto">
                 <input
                   type="radio"
                   name="service"
@@ -58,10 +55,10 @@ const ServicePage: React.FC = () => {
                   className="form-radio h-5 w-5 text-[#014F86] rounded-full border-gray-300 focus:ring-[#FC9B89]"
                 />
                 <div className="ml-4">
-                  <h2 className="text-lg font-semibold text-[#014F86]">Déménagement</h2>
+                  <h2 className="text-lg md:text-xl font-semibold text-[#014F86]">Déménagement</h2>
                 </div>
               </div>
-              <div className="w-24 h-24 md:ml-4 border-2 border-[#014F86] rounded-xl overflow-hidden">
+              <div className="w-20 h-20 md:w-24 md:h-24 mt-4 md:mt-0 md:ml-4 border-2 border-[#014F86] rounded-xl overflow-hidden">
                 <img
                   src="../images/demenagement.jpg"
                   alt="Déménagement"
@@ -71,6 +68,7 @@ const ServicePage: React.FC = () => {
             </motion.label>
           </Link>
 
+          {/* Option Nettoyage */}
           <Link href="../nettoyage" passHref>
             <motion.label
               className={`flex flex-col md:flex-row items-center justify-between p-4 border-2 ${
@@ -82,7 +80,7 @@ const ServicePage: React.FC = () => {
               whileHover="hover"
               onClick={() => handleOptionSelect('nettoyage')}
             >
-              <div className="flex items-center">
+              <div className="flex items-center w-full md:w-auto">
                 <input
                   type="radio"
                   name="service"
@@ -92,10 +90,10 @@ const ServicePage: React.FC = () => {
                   className="form-radio h-5 w-5 text-[#014F86] rounded-full border-gray-300 focus:ring-[#FC9B89]"
                 />
                 <div className="ml-4">
-                  <h2 className="text-lg font-semibold text-[#014F86]">Nettoyage</h2>
+                  <h2 className="text-lg md:text-xl font-semibold text-[#014F86]">Nettoyage</h2>
                 </div>
               </div>
-              <div className="w-24 h-24 md:ml-4 border-2 border-[#014F86] rounded-xl overflow-hidden">
+              <div className="w-20 h-20 md:w-24 md:h-24 mt-4 md:mt-0 md:ml-4 border-2 border-[#014F86] rounded-xl overflow-hidden">
                 <img
                   src="../images/nettoyage.jpg"
                   alt="Nettoyage"
@@ -105,6 +103,7 @@ const ServicePage: React.FC = () => {
             </motion.label>
           </Link>
 
+          {/* Option Transport */}
           <Link href="/transport" passHref>
             <motion.label
               className={`flex flex-col md:flex-row items-center justify-between p-4 border-2 ${
@@ -116,7 +115,7 @@ const ServicePage: React.FC = () => {
               whileHover="hover"
               onClick={() => handleOptionSelect('transport')}
             >
-              <div className="flex items-center">
+              <div className="flex items-center w-full md:w-auto">
                 <input
                   type="radio"
                   name="service"
@@ -126,10 +125,10 @@ const ServicePage: React.FC = () => {
                   className="form-radio h-5 w-5 text-[#014F86] rounded-full border-gray-300 focus:ring-[#FC9B89]"
                 />
                 <div className="ml-4">
-                  <h2 className="text-lg font-semibold text-[#014F86]">Transport</h2>
+                  <h2 className="text-lg md:text-xl font-semibold text-[#014F86]">Transport</h2>
                 </div>
               </div>
-              <div className="w-24 h-24 md:ml-4 border-2 border-[#014F86] rounded-xl overflow-hidden">
+              <div className="w-20 h-20 md:w-24 md:h-24 mt-4 md:mt-0 md:ml-4 border-2 border-[#014F86] rounded-xl overflow-hidden">
                 <img
                   src="../images/transport.jpg"
                   alt="Transport"
@@ -139,6 +138,7 @@ const ServicePage: React.FC = () => {
             </motion.label>
           </Link>
 
+          {/* Option "Non" */}
           <Link href="/" passHref>
             <motion.label
               className={`flex flex-col md:flex-row items-center justify-between p-4 border-2 ${
@@ -150,7 +150,7 @@ const ServicePage: React.FC = () => {
               whileHover="hover"
               onClick={() => handleOptionSelect('non')}
             >
-              <div className="flex items-center">
+              <div className="flex items-center w-full md:w-auto">
                 <input
                   type="radio"
                   name="service"
@@ -160,7 +160,7 @@ const ServicePage: React.FC = () => {
                   className="form-radio h-5 w-5 text-[#014F86] rounded-full border-gray-300 focus:ring-[#FC9B89]"
                 />
                 <div className="ml-4">
-                  <h2 className="text-lg font-semibold text-[#014F86]">Non, merci</h2>
+                  <h2 className="text-lg md:text-xl font-semibold text-[#014F86]">Non, merci</h2>
                 </div>
               </div>
             </motion.label>
