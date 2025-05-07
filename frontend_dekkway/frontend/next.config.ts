5613d54a2eb2b5da17a767b70caed74112c03a6d
El hadji <elhadjifallou070@gmail.com>
2025-05-07 16:51:18 +0000

    configue etabli

--- frontend_dekkway/frontend/next.config.ts
+++ frontend_dekkway/frontend/next.config.ts
@@ -1,8 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
-    domains: ['127.0.0.1', 'localhost'],
+    domains: ['127.0.0.1', 'localhost', 'api.dekkway.com'], // Ajoutez votre domaine de production ici
+  },
+  env: {
+    API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
